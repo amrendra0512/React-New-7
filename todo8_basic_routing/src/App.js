24fe881d99c7c75684d03652b1e58dd9072b1f18
amrendra0512 <amrendrakumars218@gmail.com>
2024-03-16 20:27:20 +0530

    main changed

--- todo8_basic_routing/src/App.js
+++ todo8_basic_routing/src/App.js
@@ -1,11 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-// import App from "./App";
-import NotFound from "./components/NotFound";
 import MainComponents from "./components/MainComponents";
 import Routing from "./components/Routing";
 
