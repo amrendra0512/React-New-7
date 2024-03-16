a2894796f281989a20db82d0a4e4e45d7815483a
amrendra0512 <amrendrakumars218@gmail.com>
2024-03-16 14:59:06 +0530

    for uncontrolled comp

---
+++ todo7_forms_uncontrolled/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Uncontrolled from './components/Uncontrolled';
+
+function App() {
+  return (
+    <div className='main-Content'>
+      <Uncontrolled/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo7_forms_uncontrolled/src/components/Uncontrolled.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+
+const Uncontrolled = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(emailRef.current.value, passwordRef.current.value); // the value is getting like this using useRef
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" name="" id="em" ref={emailRef} /> <br />
+      <br />
+      <input type="password" name="" id="pwd" ref={passwordRef} /> <br />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Uncontrolled;
